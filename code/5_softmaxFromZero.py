df65b456ed09c3a0d4f6b8a9d09ea774d3c407a3
xiebingshu <xiebingshu2021@163.com>
2023-07-24 19:17:25 +0800

    2023年7月24日

---
+++ code/5_softmaxFromZero.py
@@ -0,0 +1,142 @@
+import torch
+from IPython import display
+from d2l import torch as d2l
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+num_inputs = 784
+num_outputs = 10
+W = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
+B = torch.zeros(num_outputs, requires_grad=True)
+
+
+# 定义softmax函数
+def softmax(X):
+    X_emp = torch.exp(X)
+    print(X_emp)
+    partition = X_emp.sum(1, keepdim=True)
+    print(partition)
+    return X_emp / partition
+
+
+# 定义模型
+def net(X):
+    return softmax(torch.matmul(X.reshape(-1, W.shape[0]), W) + B)
+
+
+# 定义交叉熵损失函数
+def cross_entropy(y_hat, y):
+    return -torch.log(y_hat[range(len(y_hat)), y])
+
+
+# 计算准确率
+def accuracy(y_hat, y):
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+        cmp = y_hat.type(y.dtype) == y
+        return float(cmp.type(y.dtype).sum())
+
+
+class Accumulator:
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+
+def evaluate_accuracy(net, data_iter):
+    if isinstance(net, torch.nn.Module):
+        net.eval()
+    metric = Accumulator(2)
+    for X, y in data_iter:
+        metric.add(accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
+
+
+def train_epoch(net, train_iter, loss, updater):
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    metric = Accumulator(3)
+    for X, y in train_iter:
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance((updater, torch.optim.optimizer)):
+            updater.zero_grad()
+            l.backward()
+            updater.step()
+            metric.add(float(l) * len(y), accuracy(y_hat, y), y.size().numel())
+        else:
+            l.sum().backward()
+            updater(X.shape[0])
+            metric.add(float(l) * len(y), accuracy(y_hat, y), y.size().numel())
+        return metric[0] / metric[2], metric[1], metric[2]
+
+
+class Animator:
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None, ylim=None, xscale='linear', yscale='linear', \
+                 fmts=('-', 'm-', 'g-', 'r:'), nrows=1, ncols=1, figsize=(10, 6)):
+        if legend is None:
+            legend = []
+        d2l.use_svg_display()
+        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        self.config_axes = lambda: d2l.set_axes(self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        if not hasattr(y, '__len__'):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, '__len__'):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        d2l.plt.draw()
+        d2l.plt.pause(0.001)
+        display.display(self.fig)
+        display.clear_output(wait=True)
+
+    def show(self):
+        display.display(self.fig)
+
+
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
+                        legend=['train_loss', 'train_acc', 'test_acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+    train_loss, train_acc = train_metrics
+    assert train_loss < 0.5, train_loss
+    assert train_loss <= 1 and train_acc > 0.7, train_loss
+    assert train_loss <= 1 and test_acc > 0.7, test_acc
+    # animator.show()
+
+
+lr = 0.1
+
+
+def updater(batch_size):
+    return d2l.sgd([W, B], lr, batch_size)
+
+
+num_epochs = 10
+train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
