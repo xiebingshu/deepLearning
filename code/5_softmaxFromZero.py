6a1c5e4b355e7b719e96ac10d4fc9c548534ca6d
xiebingshu <xiebingshu2021@163.com>
2023-07-25 12:30:00 +0800

    Update 5_softmaxFromZero.py

--- code/5_softmaxFromZero.py
+++ code/5_softmaxFromZero.py
@@ -1,6 +1,58 @@
+
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 import torch
 from IPython import display
 from d2l import torch as d2l
+
+import matplotlib.pyplot as plt
+
+
+class Animator:  # @save
+    """在动画中绘制数据"""
+
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        # 增量地绘制多条线
+        if legend is None:
+            legend = []
+        d2l.use_svg_display()
+        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        # 使用lambda函数捕获参数
+        self.config_axes = lambda: d2l.set_axes(
+            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        # 向图表中添加多个数据点
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        plt.draw()
+        plt.pause(0.001)
+        display.display(self.fig)
+
+        display.clear_output(wait=True)
+
+
 batch_size = 256
 train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
 num_inputs = 784
@@ -66,55 +118,16 @@ def train_epoch(net, train_iter, loss, updater):
     for X, y in train_iter:
         y_hat = net(X)
         l = loss(y_hat, y)
-        if isinstance((updater, torch.optim.optimizer)):
+        if isinstance(updater, torch.optim.Optimizer):
             updater.zero_grad()
             l.backward()
             updater.step()
-            metric.add(float(l) * len(y), accuracy(y_hat, y), y.size().numel())
+            metric.add(float(l.sum()) * len(y), accuracy(y_hat, y), y.size().numel())
         else:
             l.sum().backward()
             updater(X.shape[0])
-            metric.add(float(l) * len(y), accuracy(y_hat, y), y.size().numel())
+            metric.add(float(l.sum()) * len(y), accuracy(y_hat, y), y.size().numel())
         return metric[0] / metric[2], metric[1], metric[2]
-
-
-class Animator:
-    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None, ylim=None, xscale='linear', yscale='linear', \
-                 fmts=('-', 'm-', 'g-', 'r:'), nrows=1, ncols=1, figsize=(10, 6)):
-        if legend is None:
-            legend = []
-        d2l.use_svg_display()
-        self.fig, self.axes = d2l.plt.subplots(nrows, ncols, figsize=figsize)
-        if nrows * ncols == 1:
-            self.axes = [self.axes, ]
-        self.config_axes = lambda: d2l.set_axes(self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
-        self.X, self.Y, self.fmts = None, None, fmts
-
-    def add(self, x, y):
-        if not hasattr(y, '__len__'):
-            y = [y]
-        n = len(y)
-        if not hasattr(x, '__len__'):
-            x = [x] * n
-        if not self.X:
-            self.X = [[] for _ in range(n)]
-        if not self.Y:
-            self.Y = [[] for _ in range(n)]
-        for i, (a, b) in enumerate(zip(x, y)):
-            if a is not None and b is not None:
-                self.X[i].append(a)
-                self.Y[i].append(b)
-        self.axes[0].cla()
-        for x, y, fmt in zip(self.X, self.Y, self.fmts):
-            self.axes[0].plot(x, y, fmt)
-        self.config_axes()
-        d2l.plt.draw()
-        d2l.plt.pause(0.001)
-        display.display(self.fig)
-        display.clear_output(wait=True)
-
-    def show(self):
-        display.display(self.fig)
 
 
 def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
